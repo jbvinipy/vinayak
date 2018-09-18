224ca2e8a7d33d271bb11a42483f6441a6bf834f
jbvinipy <43162681+jbvinipy@users.noreply.github.com>
2018-09-18 10:22:43 +0530

    Add files via upload

---
+++ bunking.py
@@ -0,0 +1,8 @@
+while True:
+	i=input("enter 'n' to quite")
+	if(i=='n'):
+		print("bye!")
+		exit()
+
+	else:
+		print("try again")
---
+++ table.py
@@ -0,0 +1,7 @@
+#ask the user for a number and
+#print the table of that numbe till 20
+a=int(input("give a number"))
+b=1
+while(b<=20):
+	n=a*b
+	print(a,"*",b,
